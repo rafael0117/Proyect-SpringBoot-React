1c226efa973a7a7fdb7b4319f178757d64c4fac6
rafael0117 <mamanirafael324@gmail.com>
2025-08-02 01:04:50 -0500

    Init commit

---
+++ src/main/java/com/rafael0117/SpringBoot_react/SpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.rafael0117.SpringBoot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactApplication.class, args);
+	}
+
+}
