68272d28aed842488b7dd99f05f0eb1687e58dc7
rafael0117 <mamanirafael324@gmail.com>
2025-08-07 12:20:26 -0500

    CrossOrigin

--- src/main/java/com/rafael0117/SpringBoot_react/ClienteController.java
+++ src/main/java/com/rafael0117/SpringBoot_react/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/cliente")
 public class ClienteController {
     @Autowired
